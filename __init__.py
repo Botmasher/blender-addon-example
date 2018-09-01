7db479400bcd284df7d4d22176e9fc6f6e6602d5
Botmasher <languagejosh@hotmail.com>
2018-09-01 07:24:42 -0700

    Import from module and register all classes

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
 import bpy
 import os
+import inspect
+from bpy.utils import register_class, unregister_class
+from . import addon
 
 # run from same dir as Blender file instead of app addons dir
+# otherwise move to addon dir in (MacOS) /Applications/blender.app/Contents/X.xx/scripts/
 file = os.path.join(os.path.dirname(bpy.data.filepath), "addon.py")
 exec(compile(open(file)).read(), file, 'exec')
 
@@ -14,3 +18,21 @@ bl_info = {
     'description': 'Demo of the structure of a Blender add-on',
     'category': 'Development'
 }
+
+def register_modules(modules, unregister=False):
+    for module in in modules:
+        for member in inspect.getmembers(module, inspect.inclass):
+            memberClass = member[1]
+            try:
+                registration = unregister_class(memberClass) if unregister else register_class(memberClass)
+            except RuntimeError:
+                pass
+    return
+
+def register():
+	register_modules([addon])
+
+def unregister():
+	register_modules(reverse([addon]), unregister=True)
+
+__name__ == '__main__' and register()
